bf4383bd38a67ecae195eff0982b40e33824c572
Hans Kindberg <hans.kindberg@gmail.com>
2013-04-30 15:52:12 +0200

    More implementations

---
+++ HansKindberg.EPiServer.Cms7.Abstractions/Core/IContentSource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace EPiServer.Core
+{
+	public interface IContentSource
+	{
+		#region Properties
+
+		IContent CurrentContent { get; }
+
+		#endregion
+
+		#region Methods
+
+		T Get<T>(ContentReference contentLink) where T : IContentData;
+		IEnumerable<T> GetChildren<T>(ContentReference contentLink) where T : IContentData;
+
+		#endregion
+	}
+
+	public interface IContentSource<out T> : IContentSource where T : IContent
+	{
+		#region Properties
+
+		new T CurrentContent { get; }
+
+		#endregion
+	}
+}
