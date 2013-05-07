39388c68562047efc838418323602f5b1239e590
Hans Kindberg <hans.kindberg@gmail.com>
2013-05-07 09:11:47 +0200

    Implemented TypeMismatchException

---
+++ HansKindberg.EPiServer.Cms7.Abstractions/Core/TypeMismatchException .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+namespace EPiServer.Core
+{
+	[SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable")]
+	public class TypeMismatchException : Exception
+	{
+		#region Constructors
+
+		public TypeMismatchException() {}
+		public TypeMismatchException(string message) : base(message) {}
+		public TypeMismatchException(string message, Exception innerException) : base(message, innerException) {}
+		protected TypeMismatchException(SerializationInfo info, StreamingContext context) : base(info, context) {}
+
+		#endregion
+	}
+}
